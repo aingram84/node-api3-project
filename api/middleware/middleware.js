7ceee341c301ad509561004b9867750c6dbd3f62
Adam Ingram <kr0nai84@gmail.com>
2024-04-04 13:47:10 -0700

    Main functions written

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,47 @@
+const User = require('../users/users-model');
+
 function logger(req, res, next) {
-  // DO YOUR MAGIC
+  const timeStamp = new Date().toLocaleString();
+  const method = req.method;
+  const url = req.originalUrl;
+  console.log(`[${timeStamp}] ${method} to ${url}`);
+  next();
 }
 
-function validateUserId(req, res, next) {
-  // DO YOUR MAGIC
+async function validateUserId(req, res, next) {
+  try {
+    const currentUser = await User.getById(req.params.id)
+    if (!currentUser) {
+      res.status(404).json({message: 'user not found'})
+    } else {
+      req.user = currentUser
+      next()
+    }
+  } catch (error) {
+    res.status(500).json({message: `There was an error finding the user:  ${error.message}`})
+  } next()
 }
 
 function validateUser(req, res, next) {
-  // DO YOUR MAGIC
+  const {name} = req.body
+  if (!name || !name.trim()) {
+    res.status(400).json({message: 'missing required name field'})
+  } else {
+    req.name = name.trim()
+    next()
+  }
 }
 
 function validatePost(req, res, next) {
-  // DO YOUR MAGIC
+  const {text} = req.body
+  if (!text || !text.trim()) {
+    res.status(400).json({message: 'missing required text field'})
+  } else {
+    req.text = text.trim()
+    next()
+  }
 }
+
+module.exports = {logger, validateUserId, validateUser, validatePost};
 
 // do not forget to expose these functions to other modules
--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // require your server and launch it
+const server = require('./api/server');
+require('colors');
+const port = 9000;
+server.listen(port, () => {
+    console.log(`Server listening on Port: ${port}`)
+})
